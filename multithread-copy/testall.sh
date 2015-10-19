813f1f7117392b88256fb1cb5fe3982f2c1d9cdb
Суханов Максим <sukhanov@gollard.ru>
2015-10-19 18:05:26 +0300

    Облегчён запуск всех вариантов

---
+++ multithread-copy/testall.sh
@@ -0,0 +1,26 @@
+./test.py ./mt_1 .config_no_copy_no_mempool_no_jemalloc 100
+./test.py ./mt_1 .config_no_copy_no_mempool_jemalloc 100
+./test.py ./mt_1 .config_no_copy_mempool_no_jemalloc 100
+./test.py ./mt_1 .config_no_copy_mempool_jemalloc 100
+./test.py ./mt_1 .config_copy_no_mempool_no_jemalloc 100
+./test.py ./mt_1 .config_copy_no_mempool_jemalloc 100
+./test.py ./mt_1 .config_copy_mempool_no_jemalloc 100
+./test.py ./mt_1 .config_copy_mempool_jemalloc 100
+
+./test.py ./mt_2 .config_no_copy_no_mempool_no_jemalloc 100
+./test.py ./mt_2 .config_no_copy_no_mempool_jemalloc 100
+./test.py ./mt_2 .config_no_copy_mempool_no_jemalloc 100
+./test.py ./mt_2 .config_no_copy_mempool_jemalloc 100
+./test.py ./mt_2 .config_copy_no_mempool_no_jemalloc 100
+./test.py ./mt_2 .config_copy_no_mempool_jemalloc 100
+./test.py ./mt_2 .config_copy_mempool_no_jemalloc 100
+./test.py ./mt_2 .config_copy_mempool_jemalloc 100
+
+./test.py ./mt_2 .config_no_copy_no_mempool_no_jemalloc 100
+./test.py ./mt_2 .config_no_copy_no_mempool_jemalloc 100
+./test.py ./mt_2 .config_no_copy_mempool_no_jemalloc 100
+./test.py ./mt_2 .config_no_copy_mempool_jemalloc 100
+./test.py ./mt_2 .config_copy_no_mempool_no_jemalloc 100
+./test.py ./mt_2 .config_copy_no_mempool_jemalloc 100
+./test.py ./mt_2 .config_copy_mempool_no_jemalloc 100
+./test.py ./mt_2 .config_copy_mempool_jemalloc 100
