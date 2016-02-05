77cb8ec48dbf099749e838445c425fc3160d2170
SukhanovMG <maks140693@list.ru>
2016-02-05 16:09:43 +0300

    Новая очередь. Производительность увеличилась в 2-3 раза.

--- common/tm_queue.h
+++ common/tm_queue.h
@@ -5,25 +5,28 @@
 #include <pthread.h>
 #include "tm_block.h"
 
+typedef struct {
+	tm_block *block;  // указатель на блок
+	size_t client_id; // id клиента, для которого предназначен блок
+} client_block_t;
+
 typedef struct _tm_queue_elem_ctx {
 	struct _tm_queue_elem_ctx *next;
-	unsigned int client_id;
-	tm_block *block;
+	struct _tm_queue_elem_ctx *prev;
+	client_block_t client_block;
 } tm_queue_elem_ctx;
 
 typedef struct _tm_queue_ctx {
 	pthread_mutex_t mutex;
 	pthread_cond_t cond;
 	tm_queue_elem_ctx *head;
-	tm_queue_elem_ctx *tail;
-	unsigned long long int count;
-	int finish;
+	volatile int break_flag;
 } tm_queue_ctx;
 
 tm_queue_ctx *tm_queue_create();
-int tm_queue_push_back(tm_queue_ctx *q, tm_block *block);
-int tm_queue_push_back_client_id(tm_queue_ctx *q, tm_block *block, unsigned int client_id);
-tm_queue_elem_ctx *tm_queue_pop_front(tm_queue_ctx *q);
-int tm_queue_destroy(tm_queue_ctx *q);
+void tm_queue_destroy(tm_queue_ctx *q);
+
+int tm_queue_push_back(tm_queue_ctx *q, client_block_t *client_block_array, int count);
+int tm_queue_pop_front(tm_queue_ctx *q, client_block_t *client_block_array, int count);
 
 #endif /* TM_QUEUE_H */
